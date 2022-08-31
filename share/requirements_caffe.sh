e14999109fb672c64646fc7a9af58a22fbef7045
Hoosang Lee <lhs223@gmail.com>
2022-08-31 22:41:28 +0900

    add requirements

---
+++ share/requirements_caffe.sh
@@ -0,0 +1,7 @@
+# For NVIDIA issue
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A4B469963BF863CC
+
+apt update
+pip install --upgrade pip
+apt-get install python-setuptools python-tk gedit graphviz
+pip install functools32 tqdm pydot
---
+++ share/requirements_label.sh
@@ -0,0 +1,3 @@
+apt update
+apt install -y libgl1-mesa-glx libglib2.0-0
+pip install opencv-python jupyter matplotlib
