fddf2f06d96dbb9388c45636f06894004eab589d
Hoosang Lee <lhs223@gmail.com>
2022-09-13 13:40:04 +0900

    add requirements_torch.sh

---
+++ share/requirements_torch.sh
@@ -0,0 +1,14 @@
+# For NVIDIA issue
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A4B469963BF863CC
+
+sed -i 's/kr.archive.ubuntu.com/mirror.kakao.com/g' /etc/apt/sources.list
+apt update
+
+apt install -y python3.8
+update-alternatives --install /usr/bin/python python /usr/bin/python3.8 1
+
+
+#pip3 install torch torchvision
+#apt install -y libgl1-mesa-glx libglib2.0-0
+#pip3 install opencv-python jupyter matplotlib
+
