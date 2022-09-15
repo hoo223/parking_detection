875aa2f791fa2995fefb8948a587ce0d70f21c59
Hoosang Lee <lhs223@gmail.com>
2022-09-15 13:07:29 +0900

    Update

--- launch_docker_pytorch.sh
+++ launch_docker_pytorch.sh
@@ -2,6 +2,7 @@ sudo docker run --gpus all -it --net=host --privileged \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -e DISPLAY=unix$DISPLAY \
     -v `pwd`/share:/root/share \
+    -w /root/share \
     --name parking_torch \
     nvidia/cudagl:11.4.0-devel-ubuntu18.04 \
     bash
